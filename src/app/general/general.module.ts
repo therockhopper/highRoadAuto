b6e0e568afa8108d38a0a60326e3826e1ab5fc00
Frank Schmidt <frank.schmidt@redspace.com>
2017-04-09 17:40:58 -0300

    convert to highRoad app

--- src/app/general/general.module.ts
+++ src/app/general/general.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
-import { SurveySliderComponent } from './survey-slider/survey-slider.component';
 
 @NgModule({
   imports: [
@@ -11,12 +10,10 @@ import { SurveySliderComponent } from './survey-slider/survey-slider.component';
   declarations: [
     NavBarComponent,
     FooterComponent,
-    SurveySliderComponent,
   ],
   exports: [
     NavBarComponent,
     FooterComponent,
-    SurveySliderComponent,
   ]
 })
 export class GeneralModule { }
