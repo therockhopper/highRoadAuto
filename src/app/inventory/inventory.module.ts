e3f296d77f0c470319dc1fc9a7d1366f6405af6f
Frank Schmidt <frankschmidt902@gmail.com>
2017-04-14 18:54:16 -0300

    create inventory service

--- src/app/inventory/inventory.module.ts
+++ src/app/inventory/inventory.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ClarityModule } from 'clarity-angular';
 import { RouterModule, Routes } from '@angular/router';
+import { InventoryService } from './inventory.service';
 import { ShopComponent } from './shop/shop.component';
 
 const loginRoutes: Routes = [
@@ -20,6 +21,9 @@ const loginRoutes: Routes = [
   declarations: [
     ShopComponent,
   ],
+  providers: [
+    InventoryService,
+  ]
   exports: [
     RouterModule,
     ShopComponent,
